24241b334803674da01b6912c04e5f59ae2d3ac4
Shema2 <91555105+Shema2@users.noreply.github.com>
2021-10-15 17:14:07 +0300

    Update task01.js

--- task01.js
+++ task01.js
@@ -4,4 +4,9 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
-// 
+// https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/javascript
+
+function move(position, roll) {
+  return position + roll * 2;
+}
+
